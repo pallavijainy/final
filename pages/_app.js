27dea4385d13b14322e17106f381b3870121444e
pallavi jain <pallavijainy0612@gmail.com>
2023-01-09 14:11:39 +0530

    add changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+  <Component {...pageProps} />
+  </ChakraProvider>)
 }
